dcf1ff5a185708219764b77d7a33da911baee3b3
phutharesuanwachirapisut <phutharesuan.wachirapisut@gmail.com>
2025-06-23 12:33:54 +0700

    Updated forecast.py

---
+++ ml/forecast.py
@@ -0,0 +1,12 @@
+# ml/forecast.py
+import numpy as np
+import pickle
+
+# โหลดโมเดลที่เทรนไว้
+with open("ml/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+def predict_from_input(data):
+    X = np.array(data)  # สมมติ input เป็น list of list [[x1, x2, ...], ...]
+    y_pred = model.predict(X)
+    return y_pred.tolist()
